7e5b6d7405cc3f9a948f587bca31ce92b12be808
Robert William Schlegel <robwschlegel@gmail.com>
2023-11-03 18:28:16 +0100

    Combining main function tests into one pipeline test script to avoid multiple downloads during testing.

--- tests/testthat/test-flget_PARbottomMonthlyTS.R
+++
@@ -1,6 +0,0 @@
-# tests for Monthly TS function
-
-test_that("flget_PARbottomMonthlyTS error messages signal correctly", {
-  dat_no_TS <- flget_PARbottomMonthlyTS()
-  expect_error(fl_DownloadFjord(fjord = "banana"), "banana not available")
-})
---
+++ tests/testthat/test-pipeline.R
@@ -0,0 +1,48 @@
+# Test all functions in one script to avoid multiple downloads
+
+
+# Downloading -------------------------------------------------------------
+
+test_that("fl_DownloadFjord error messages signal correctly", {
+  expect_error(fl_DownloadFjord(fjord = "banana"), "banana not available")
+})
+
+test_that("fl_DownloadFjord temp directory works and doesn't re-download", {
+  test_dl <- fl_DownloadFjord(fjord = "kong")
+  test_dl <- fl_DownloadFjord(fjord = "kong")
+  expect_type(test_dl, "NULL")
+})
+
+test_that("fl_DownloadFjord custom directory works and doesn't re-download", {
+  test_dl <- fl_DownloadFjord(fjord = "kong", dirdata = "PAR_test")
+  test_dl <- fl_DownloadFjord(fjord = "kong", dirdata = "PAR_test")
+  expect_type(test_dl, "NULL")
+})
+unlink("./PAR_test", recursive = TRUE)
+
+
+# Loading -----------------------------------------------------------------
+
+test_that("loading without TS works", {
+  test_dat <- fl_LoadFjord("kong")
+  expect_type(test_dat, "list")
+  expect_equal(length(test_dat$longitude), 321)
+  expect_equal(round(test_dat$AreaOfShallowZone), 106)
+})
+
+test_that("TS loading works", {
+  test_dat <- fl_LoadFjord("kong", TS = TRUE)
+  expect_type(test_dat, "list")
+  expect_type(test_dat$MonthlyPARbottom, "double")
+})
+
+
+# PARbottom ---------------------------------------------------------------
+
+test_that("flget_PARbottomMonthlyTS error messages signal correctly", {
+  dat_no_TS <- flget_PARbottomMonthlyTS()
+  expect_error(fl_DownloadFjord(fjord = "banana"), "banana not available")
+})
+
+# Remove test files
+unlink("./FjordLight.d", recursive = TRUE)
